aaa04c6097f3b5b1d76d048391a182d12bd19eac
Jino <abhkim0803@gmail.com>
2023-12-11 13:55:01 +0700

    Preparing app for deloyment

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+server.timeout = 120000;
 
 process.on('unhandledRejection', err => {
     console.log(err.name, "\n", err.message);
